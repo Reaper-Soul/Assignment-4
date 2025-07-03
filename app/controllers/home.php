b5fb5a794a404045faf032af0dc4372a0e1a7dc2
Navish Sharma <navishsharma2004@gmail.com>
2025-07-03 12:03:12 +0000

    added a way to check which nav link is active

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,7 +5,8 @@ class Home extends Controller {
     public function index() {
       $user = $this->model('User');
       $data = $user->test();
-			
+       
+      $_SESSION['current_page'] = 'home';
 	    $this->view('home/index');
 	    die;
     }
